b401ad440520d786bce689d27b5c5155904336e9
Julien <julien@citio.digital>
2024-06-19 02:06:32 +0200

    up

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ app.use('/', function (clientRequest, clientResponse) {
         // Make changes to HTML files when they're done being read.
          body = body.replace(`class="dorik-branding"`, `style="display:none;"`);
          body = body.replace(`class='dorik-branding'`, `style="display:none;"`);
-         body = body.replace(`https://myteslamatecom.dorik.io`, `https://www.myteslamate.com`);
-         body = body.replace(`https://myteslamatecom.dorik.io`, `https://www.myteslamate.com`);
+         body = body.replace(`https://mtm.dorik.io`, `https://www.myteslamate.com`);
+         body = body.replace(`https://mtm.dorik.io`, `https://www.myteslamate.com`);
 
          serverResponse.headers['cache-control'] = 'max-age=604800';
          delete serverResponse.headers['cf-cache-status'];
